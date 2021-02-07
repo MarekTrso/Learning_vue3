c050f22c25b3fdf43011dbf30bde5ba427589c7a
MarekTrso <marek.trso@gmail.com>
2021-02-07 12:11:20 +0100

    Snáď to pôjde

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp ({
+   data() {
+      return {
+         product: 'Socks',
+         description: 'Warm and bushy socks, ideal for winter',
+         image: '/Intro-to-Vue-3/assets/images/socks_blue.jpg'
+      };
+   }
+});
