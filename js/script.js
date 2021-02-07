17c4c32ad38606c465e71497e093f13373879ce0
MarekTrso <marek.trso@gmail.com>
2021-02-07 16:45:44 +0100

    Doplnená dostupnosť tovaru

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const app = Vue.createApp ({
    data() {
       return {
          product: 'Socks',
-         description: 'Warm and bushy socks, ideal for winter',
          image: './img/socks_blue.jpg',
-         url: 'https://www.vuemastery.com/'
+         inventory: 100,
+         onSale: false
       };
    }
 });
