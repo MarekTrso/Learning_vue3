874554213e8f2f55ca0bed58362238377d82442d
MarekTrso <marek.trso@gmail.com>
2021-02-08 11:46:35 +0100

    Počet položiek v košíku a tlačidlo pridať do košíka

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp ({
    data() {
       return {
+          cart: 0,
          product: 'Socks',
          image: './img/socks_blue.jpg',
          inventory: 100,
          onSale: false,
-         details: ['50% cotton', '30% wool', '20% polyester']
+         details: ['50% cotton', '30% wool', '20% polyester'],
+         variants: [
+             { id: 2234, color: 'green'},
+             { id: 2235, color: 'blue'}
+            ]
       };
-   }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        }
+    }
 });
