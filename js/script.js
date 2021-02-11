e3bc3b03cd238e034b508dcda1a715aa9d5a8b0b
MarekTrso <marek.trso@gmail.com>
2021-02-11 13:26:43 +0100

    Dokoncene vsetky ulohy

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 const app = Vue.createApp ({
    data() {
       return {
-          cart: 0,
-         product: 'Socks',
-         image: './img/socks_blue.jpg',
-         inventory: 100,
-         onSale: false,
-         details: ['50% cotton', '30% wool', '20% polyester'],
-         variants: [
-             { id: 2234, color: 'green', image: './img/socks_green.jpg'},
-             { id: 2235, color: 'blue', image: './img/socks_blue.jpg'}
-            ]
-      };
-    },
+         cart: [],
+         premium: true
+     };
+  },
     methods: {
-        addToCart() {
-            this.cart += 1;
+        updateCart(id) {
+            this.cart.push(id);
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
-        },
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1;
-            }
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1);
+                }
         }
     }
 });
